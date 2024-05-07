ede6b6835dcc0bf353e2e205e0b0a4dd9e611800
SHAIK ABDUL Basha K <basha.shaik.abdul@hexagon.com>
2024-05-08 05:07:06 +0530

    added asp.net mvc folders

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using myapp.Models;
+
+namespace myapp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.OSVersion = Environment.OSVersion;
+        PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        // Read the current CPU usage percentage
+        float cpuUsage = cpuCounter.NextValue();
+
+        PerformanceCounter memCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+        // Read the available memory in MB
+        float availableMemory = memCounter.NextValue();
+        
+        // Wait for a moment to get a more accurate reading
+        System.Threading.Thread.Sleep(1000);
+
+        // Read CPU usage percentage again
+        cpuUsage = cpuCounter.NextValue();
+        ViewBag.CpuUsage = cpuUsage;
+        ViewBag.MemUsage = availableMemory;
+
+        void Configure(IApplicationBuilder app)
+        {
+            // Other middleware configurations...
+
+            // Add the access log middleware
+            app.UseMiddleware<AccessLogMiddleware>("access.log");
+
+            // Other middleware configurations...
+        }
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        var log = new LogInfo(_configuration);
+
+        var data = log.LogData();
+        ViewBag.Data = data;
+
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
