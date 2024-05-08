19fe8465b187a29f85f1b71cd22d955b6a425b82
SHAIK ABDUL Basha K <basha.shaik.abdul@hexagon.com>
2024-05-08 16:30:14 +0530

    v3 changes

--- obj/Debug/net8.0/myapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede6b6835dcc0bf353e2e205e0b0a4dd9e611800")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
